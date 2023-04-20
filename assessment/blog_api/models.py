86a525a1f16f04317226fa2acdcfe00f6778996b
Yxndza <37433612+Yxndza@users.noreply.github.com>
2023-04-20 22:51:56 +0200

    Add files via upload

---
+++ assessment/blog_api/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.CharField(max_length=100)
+    published_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=100)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
